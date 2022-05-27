a344b25ec9aee8fd3eefcea6ff59cc902c0e5c31
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-05-27 14:16:44 +0900

    Implement Global Contexts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import CategoryContext, { ICategoryContext } from "../context/CategoryContext";
+import { Categories, Category } from "../types/Category";
+import { Theme } from "../containers/ThemeButton";
+import ThemeContext, { IThemeContxt } from "../context/ThemeContext";
+import "prismjs/themes/prism.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [currentCategory, setCurrentCategory] = useState<Category>(
+    Categories.about
+  );
+  const categoryValue: ICategoryContext = {
+    category: currentCategory,
+    setCategory: setCurrentCategory,
+  };
+  const [currentTheme, setCurrentTheme] = useState<Theme>("light");
+  const themeValue: IThemeContxt = {
+    theme: currentTheme,
+    setTheme: setCurrentTheme,
+  };
+
+  return (
+    <ThemeContext.Provider value={themeValue}>
+      <CategoryContext.Provider value={categoryValue}>
+        <Component {...pageProps} />
+      </CategoryContext.Provider>
+    </ThemeContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
