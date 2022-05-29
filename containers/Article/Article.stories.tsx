5abea0b31440dc055b67f82162cd812adbee858b
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-05-29 09:46:08 +0900

    Fix: arguments change

--- containers/Article/Article.stories.tsx
+++ containers/Article/Article.stories.tsx
@@ -2,6 +2,7 @@ import Article from "./Article";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { serialize } from "next-mdx-remote/serialize";
 import matter from "gray-matter";
+import { Documentation } from "../../types/IDocumentation";
 
 export default {
   title: "Containers/Article/Article",
@@ -13,19 +14,19 @@ title: Front Mater
 This is content.
 `;
 
+const docs: Documentation = {
+  name: "Hello",
+  path: "",
+  date: "Wed, 25 May 2022 05:15:54 GMT",
+  src: source,
+};
+
 const { content, data } = matter(source);
 export const Template: ComponentStory<typeof Article> = (
   args,
   { loaded: { src } }
 ) => {
-  return (
-    <Article
-      contributors={["Zuhanit", "torvalds"]}
-      title="Hello"
-      lastModified="2022.05.19"
-      src={src}
-    />
-  );
+  return <Article docs={docs} matter={data} src={src} />;
 };
 
 Template.loaders = [
