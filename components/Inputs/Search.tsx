09ce4201b37e8a66de201c11b7e0eb4b8a494d42
zuhanit <36349353+zuhanit@users.noreply.github.com>
2022-06-01 12:52:12 +0900

    fix: Add fowardRef

--- components/Inputs/Search.tsx
+++ components/Inputs/Search.tsx
@@ -1,86 +1,90 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, forwardRef, useState } from "react";
 import { useRef } from "react";
 import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai";
 
 interface SearchProps extends Omit<React.HTMLProps<HTMLInputElement>, "type"> {}
 
-const Search = (props: SearchProps) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [valueExist, setValueExist] = useState(false);
-  const onChangeSearch = (event: ChangeEvent<HTMLInputElement>) => {
-    event.target.value.length !== 0
-      ? setValueExist(true)
-      : setValueExist(false);
-    if (props.onChange) {
-      props.onChange(event);
-    }
-  };
-  const onClickReset = () => {
-    inputRef.current!.value = "";
-  };
-  return (
-    <div className="search-wrapper">
-      <style jsx>{`
-        .search-wrapper {
-          position: relative;
-          width: 100%;
-        }
+const Search = forwardRef<HTMLDivElement, SearchProps>(
+  (props: SearchProps, ref) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [valueExist, setValueExist] = useState(false);
+    const onChangeSearch = (event: ChangeEvent<HTMLInputElement>) => {
+      event.target.value.length !== 0
+        ? setValueExist(true)
+        : setValueExist(false);
+      if (props.onChange) {
+        props.onChange(event);
+      }
+    };
+    const onClickReset = () => {
+      inputRef.current!.value = "";
+    };
+    return (
+      <div className="search-wrapper" ref={ref}>
+        <style jsx>{`
+          .search-wrapper {
+            position: relative;
+            width: 100%;
+          }
 
-        input {
-          width: 100%;
-          height: 32px;
-          border-radius: 5px;
-          border: 1px solid var(--chassis);
-          padding-left: calc(15px + 7px + 5px);
-          background: var(--background);
-          color: var(--text);
-        }
+          input {
+            width: 100%;
+            height: 32px;
+            border-radius: 5px;
+            border: 1px solid var(--chassis);
+            padding-left: calc(15px + 7px + 5px);
+            background: var(--background);
+            color: var(--text);
+          }
 
-        :global(.search-icon) {
-          position: absolute;
-          left: 7px;
-          top: 50%;
-          transform: translateY(-50%);
-        }
+          :global(.search-icon) {
+            position: absolute;
+            left: 7px;
+            top: 50%;
+            transform: translateY(-50%);
+          }
 
-        :global(.search-delete-icon) {
-          position: absolute;
-          right: 7px;
-          top: 50%;
-          transition: 0.3s ease;
-          transform: scale(0.8) translateY(-50%);
-          cursor: pointer;
-        }
+          :global(.search-delete-icon) {
+            position: absolute;
+            right: 7px;
+            top: 50%;
+            transition: 0.3s ease;
+            transform: scale(0.8) translateY(-50%);
+            cursor: pointer;
+          }
 
-        :global(.visible) {
-          opacity: 1;
-          transform: scale(1) translateY(-50%);
-          will-change: transform;
-        }
+          :global(.visible) {
+            opacity: 1;
+            transform: scale(1) translateY(-50%);
+            will-change: transform;
+          }
 
-        :global(.hidden) {
-          opacity: 0;
-        }
-      `}</style>
-      <input
-        {...props}
-        type="text"
-        ref={inputRef}
-        onChange={onChangeSearch}
-      ></input>
-      <AiOutlineSearch
-        className="search-icon"
-        size="15px"
-        fill="var(--chassis)"
-      />
-      <AiOutlineClose
-        className={`search-delete-icon ${valueExist ? "visible" : "hidden"}`}
-        size="15px"
-        fill="var(--chassis)"
-        onClick={onClickReset}
-      />
-    </div>
-  );
-};
+          :global(.hidden) {
+            opacity: 0;
+          }
+        `}</style>
+        <input
+          {...props}
+          type="text"
+          ref={inputRef}
+          onChange={onChangeSearch}
+        ></input>
+        <AiOutlineSearch
+          className="search-icon"
+          size="15px"
+          fill="var(--chassis)"
+        />
+        <AiOutlineClose
+          className={`search-delete-icon ${valueExist ? "visible" : "hidden"}`}
+          size="15px"
+          fill="var(--chassis)"
+          onClick={onClickReset}
+        />
+      </div>
+    );
+  }
+);
+
+Search.displayName = "Search";
 
 export default Search;
